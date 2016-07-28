9b26b718189340557dfed25c6e7c7f77f76897df
Frank Edwards <fedwards@uw.edu>
2016-07-28 12:50:00 -0700

    add guardian data

--- models.r
+++ models.r
@@ -1,2 +1,9 @@
-dat<-read.csv("~/data-police-shootings/fatal-police-shootings-data.csv")
+library(dplyr)
+library(ggplot2)
+setwd("~/data-police-shootings/")
+wapo<-read.csv("fatal-police-shootings-data.csv")
+c1<-read.csv("the-counted-2015.csv")
+c2<-read.csv("the-counted-2016.csv")
+guard<-rbind(c1, c2)
+
 m1<-glmer(armed!="unarmed"~(race=="B")+(1|state), data=dat, family="binomial")
